17ce5b52ba53318044b01d1c5643c345661de8b1
Suyash Kumar <suyashkumar009@gmail.com>
2023-03-29 20:47:57 +0530

    Added Routing for about page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { useState } from "react";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import { Routes, Route } from "react-router";
+
 function App() {
   //State
   const [mode, setMode] = useState("light");
@@ -23,20 +25,36 @@ function App() {
   };
 
   return (
-    <>
-      <Navbar
-        mode={mode}
-        title="Text Utilities"
-        homeText="Home"
-        aboutText="About"
-        toggleMode={toggleMode}
-        btnText={btnText}
-      />
-      <div className="container my-3">
-        <TextForm heading="Converter" toggleMode={mode} />
-      </div>
-      {/* <About></About> */}
-    </>
+    <Routes>
+      <Route path="/" element={
+        <div>
+          <Navbar
+            mode={mode}
+            title="Text Utilities"
+            homeText="Home"
+            aboutText="About"
+            toggleMode={toggleMode}
+            btnText={btnText}
+          />
+          <div className="container my-3">
+            <TextForm heading="Converter" toggleMode={mode} />
+          </div>
+        </div>
+      } />
+      <Route path="/about" element={
+        <div>
+          <Navbar
+            mode={mode}
+            title="Text Utilities"
+            homeText="Home"
+            aboutText="About"
+            toggleMode={toggleMode}
+            btnText={btnText}
+          />
+          <About />
+        </div>
+      } />
+    </Routes>
   );
 }
 
